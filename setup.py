40b035fadbec1b6f11fdfc398dc6942b6f972ff0
sperrard <stephane.perrard@espci.fr>
2023-04-20 15:03:43 +0200

    first codes

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='stephane_codes',
+    version='0.1',
+    description='Misc utilities (Stephane Perrard)',
+#      url='needs a URL',
+    author='Stephane Perrard',
+    author_email='stephane.perrard@espci.fr',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+#      package_data={'tangle': ['cl_src/*.cl']})
+)
