b93f545806f2825ab7e7ffc200310e397a3c97a1
BaptisteAuvity <baptiste.auvity@gmail.com>
2023-05-23 19:16:15 +0200

    icewave/

--- icewave/geophone/extract_triangles_flexion.py
+++ icewave/geophone/extract_triangles_flexion.py
@@ -7,9 +7,6 @@ Created on Mon May 22 14:51:57 2023
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import medfilt2d
-from scipy.signal import find_peaks
-from skimage.measure import profile_line
 import scipy.fft as fft
 import scipy.io as io
 import os
@@ -42,14 +39,10 @@ savefolder = 'W:\Banquise\Rimouski_2023\Traitements_donnees\\baptsarahantonin\\'
 
 if not os.path.exists(savefolder):
     os.makedirs(savefolder)
-# sur adour
-#base = '/media/turbots/DATA/thiou/labshared2/'
 
 filelist = glob.glob(folder+'*.txt')
 print(filelist)
-#230313_1714_048.txt'
 filename = filelist[3]
-#print(filename)
 
 name = os.path.basename(filename).split('.')[0]
 
@@ -100,12 +93,6 @@ for i in range (0, int(size/params['lm'])) :
             
             corr = correlate(Y1,Y2)
             
-            peaks, u = find_peaks(corr)
-            
-            diff_peaks = peaks[1:] - peaks[:-1]
-            
-            period = np.median(diff_peaks)
-            
             lag = np.argmax(corr)
             
             a = 5
@@ -119,42 +106,61 @@ for i in range (0, int(size/params['lm'])) :
             maxmax = -p[1]/(2*p[0])
             
             lag = lag + maxmax - a - len(corr)/2
+            
+            period = params['lm'] / j
                 
-            phi = lag / (period * 2 * np.pi)
+            phi = lag / period * 2 * np.pi
             
             params['phi'][i,j] = phi
             
 
-        
         if method == 2 :
+            
             demod1 = demodulation(t, data_cut[:,0], j)
             demod2 = demodulation(t, data_cut[:,1], j)
             
 
 
 #%% Phase shift et histogram
+save = False
 
+params = disp.figurejolie(params = params, nom_fig = 'phase_shift_f_ttmorceaux')
 phase_shift = params['phi']
-   
-phase_shift[phase_shift< -np.pi] += 2 * np.pi
-phase_shift[phase_shift> np.pi] += - 2 * np.pi
 
-params = disp.figurejolie(params, nom_fig = 'phase_shift')
-f = np.linspace(1, np.shape(phase_shift)[1], np.shape(phase_shift)[1]) / np.shape(data_cut)[0] * 1000
-n_phase = np.linspace(0, np.shape(phase_shift)[0], np.shape(phase_shift)[0])
+params['n_morc'] = np.shape(phase_shift)[0]
+params['n_f'] = np.shape(phase_shift)[1]
+
+while phase_shift[phase_shift> np.pi] != np.array([]) or phase_shift[phase_shift< -np.pi] != np.array([]) :
+    phase_shift[phase_shift< -np.pi] += 2 * np.pi
+    phase_shift[phase_shift> np.pi] += - 2 * np.pi
+
+f = np.linspace(1, params['n_f'], params['n_f']) / params['lm'] * params['facq']
+for u in range (0, 180):
+    plt.plot(f, phase_shift[u,:], '+')
+
+if save :
+    sv.save_graph(savefolder, 'phase_shift_f_ttmorceaux', params = params)
+
+
+
+params = disp.figurejolie(params, nom_fig = 'phase_shift_f_morceaux')
+
+n_phase = np.linspace(0, params['n_morc'], params['n_morc'])
 
 params[str(params['num_fig'][-1])]['data'] = disp.joliplot( 'f','n_phase', f, n_phase ,color = 5, table = np.flip(np.rot90(phase_shift), 0))
 
-# sv.save_graph(savefolder, 'phase_f_morceau', params = params)
+if save :
+    sv.save_graph(savefolder, 'phase_f_morceau', params = params)
 
 
 params['n_bins'] = 24
-histogram = np.zeros(( params['n_bins'], np.shape(phase_shift)[1]))
+histogram = np.zeros(( params['n_bins'], params['n_f']))
 
 params = disp.figurejolie(params = params, nom_fig = 'histogram')
 
-for k in range (0,np.shape(phase_shift)[1]) :
-    print(k)
+for k in range (0,params['n_f']) :
+    if np.mod(k, 50) == 0:
+        print(k)
     
     h = plt.hist(phase_shift[:,k], params['n_bins'])
     
@@ -167,75 +173,107 @@ for k in range (0,np.shape(phase_shift)[1]) :
 params = disp.figurejolie(params = params, nom_fig = 'phase_shift_histogrammé')
 boites = np.linspace(0,params['n_bins'],params['n_bins'])
 params[str(params['num_fig'][-1])]['data'] = disp.joliplot( 'f','n_phase', f, boites, table = np.flip(np.rot90(histogram), 0))
+plt.clim(0,30)
 
-# sv.save_graph(savefolder, 'phase_shift_histogram', params = params)
+if save :
+    sv.save_graph(savefolder, 'phase_shift_histogram', params = params)
 
 #%% Detect points
+save = False
 
-test_detect = np.concatenate((np.flip(histogram, 0),np.flip(histogram, 0),np.flip(histogram, 0),np.flip(histogram, 0)), axis = 0)
+params['nb_blocks'] = 4
+
+test_detect = np.flip(histogram, 0)
+for i in range (0, params['nb_blocks'] - 1) :
+    test_detect = np.concatenate((test_detect, np.flip(histogram, 0)), axis = 0)
 
 params = disp.figurejolie(params = params, nom_fig = 'phase_shift_histogrammé_fois4')
 
-plt.pcolormesh(test_detect)
+len_phase = np.linspace(-np.pi, 2 * np.pi * params['nb_blocks'] - np.pi, params['nb_blocks'] * params['n_bins'])
+plt.pcolormesh(f,len_phase, test_detect)
 cbar = plt.colorbar()
 plt.clim(0,30)
 
 Y = fft.fft(test_detect, axis = 0)
 
-phase = - np.angle(Y[1,:])
+phase = - np.angle(Y[params['nb_blocks'],:])
 
-phase = np.unwrap(phase)+ 2 * np.pi
+phase = np.unwrap(phase) - np.pi + 2* np.pi
+
+params[str(params['num_fig'][-1])]['data'] = disp.joliplot( 'f','phase', f, phase, color = 2, exp = False)
+
+if save :
+    sv.save_graph(savefolder, 'phase_en_fonction_de_f_branches_plus_detection', params = params)
 
 params = disp.figurejolie(params = params, nom_fig = 'phase_en_fonction_de_f')
-params[str(params['num_fig'][-1])]['data'] = disp.joliplot( 'f','phase', f, phase, color = 4, exp = False)
-sv.save_graph(savefolder, 'phase_en_fonction_de_f', params = params)
+params[str(params['num_fig'][-1])]['data'] = disp.joliplot( 'f','phase', f, phase, color = 2, exp = False)
 
-#%% test
-
-params = disp.figurejolie(params = params, nom_fig = 'phase_en_fonction_de_f')
-Y = fft.fft(test_detect, axis = 0)
-
-for i in range (0,Y.shape[0]):
-    print(i)
-    phi = - np.angle(Y[i,:])
-    
-    phi = np.unwrap(phi) 
-    
-    plt.plot(f, phi)
-    
+if save :
+    sv.save_graph(savefolder, 'phase_en_fonction_de_f_branche_detectée', params = params)
+   
 #%% Unité physique
-
-phase[110:230] = phase[110:230] + 2 * np.pi
-
+save = False
+# phase[110:230] = phase[110:230] + 2 * np.pi
 
 params['L'] = 20
 params['theta'] = 0
-params['f_coup'] = 159
+params['f_coup'] = 135
 
 omega = f[:params['f_coup']] * 2 * np.pi
 
 k = phase[:params['f_coup']] / params['L'] /np.cos(params['theta'])
 
-plt.figure()
-plt.plot(k, omega)
+params = disp.figurejolie(params = params, nom_fig = 'k_de_omega')
+params[str(params['num_fig'][-1])]['data'] = disp.joliplot( r'k ($m^{-1}$)',r'$\omega$ (Hz)', k, omega, color = 4, exp = False)
 
+if save :
+    sv.save_graph(savefolder, 'k_de_omega', params = params)
+    
 #%% FIT
+save = False
+save_matlab = False
 
 import baptiste.math.fits as fit
 import baptiste.math.RDD as rdd
 
-popt, pcov = fit.fit(rdd.RDD_flexion, k, omega, display = True, err = False, nb_param = 1, p0 = [6e6], bounds = False, zero = True, th_params = False, xlabel = r'k (m$^{-1}$)', ylabel = r'$\omega$')
+type_fit = 'pesante_flexion'
+
+
+if type_fit == 'pesante_flexion':
+    params = disp.figurejolie(params = params, nom_fig = type_fit)
+    popt, pcov = fit.fit(rdd.RDD_pesante_flexion, k, omega, display = True, err = False, nb_param = 2, p0 = [0.4, 0], bounds = [[0,1],[0,50e10]], zero = True, th_params = False, xlabel = r'k (m$^{-1}$)', ylabel = r'$\omega$')
+    params[str(params['num_fig'][-1])]['hxrho'] = popt[0]
+    params[str(params['num_fig'][-1])]['Dsurrho'] = popt[1]
+    params[str(params['num_fig'][-1])]['erreurfit'] = pcov
+    params[str(params['num_fig'][-1])]['data'] = sv.data_to_dict(['k','omega'], [k,omega], data = [k,omega])
+    if save :
+        sv.save_graph(savefolder, type_fit, params = params)
+
+type_fit = 'power_law'
+if type_fit == 'power_law':
+    params = disp.figurejolie(params = params, nom_fig = 'k_de_omega')
+    popt = fit.fit_powerlaw(k[30:],omega[30:], display = True, xlabel = '', ylabel = '', legend = '')
+    params[str(params['num_fig'][-1])]['power'] = popt
+    params[str(params['num_fig'][-1])]['data'] = sv.data_to_dict(['k','omega'], [k,omega], data = [k,omega])
+    if save :
+        sv.save_graph(savefolder, type_fit, params = params)
+      
+if save_matlab :
+    sv.save_mat(k, savefolder, title = 'k')
+    sv.save_mat(k, savefolder, title = 'omega')
+    full_params = {}
+    for i in params.keys() :
+        if not i.isdigit() :
+            full_params[i] = params[i]
+    sv.save_mat(full_params, savefolder, title = 'parametres')
 
 
 
 
+#%% SAVE ALL
 
 
-
-
-
-
-
+sv.save_all_figs(savefolder, params)
 
 
 
