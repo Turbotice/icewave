c6647ce956b08a4398f0140815c0401017d94399
sperrard <stephane.perrard@espci.fr>
2024-02-10 21:57:23 -0500

    Modify geometry codes

--- icewave/geometry/define.py
+++ icewave/geometry/define.py
@@ -16,24 +16,46 @@ instruments = {'G':'Geophones',\
 def gen_point(num,x0,y0,z0,instrument='G'):
     return [instrument+'_'+ndigit(num),x0,y0,z0]
 
-def gen_sources(D,L,n,n0=1,x0=0,y0=0,z0=0,axis=0):
+def add_sources(Dlist,n0,x0,y0,z0,sign=1,axis=0):
     lines = []
-    if axis==0:
-        lines.append(gen_point(n0,x0-D,y0,z0,instrument='S'))
-        lines.append(gen_point(n0+1,x0+(n-1)*L+D,y0,z0,instrument='S'))
-    if axis==1:
-        lines.append(gen_point(n0,x0,y0-D,z0,instrument='S'))
-        lines.append(gen_point(n0+1,x0,y0+(n-1)*L+D,z0,instrument='S'))
+    for i,D in enumerate(Dlist):
+        if axis==0:
+            lines.append(gen_point(n0+i,x0-D*sign,y0,z0,instrument='S'))
+        if axis==1:
+            lines.append(gen_point(n0+i,x0,y0-D*sign,z0,instrument='S'))
     return lines
 
-def gen_line(n,L,n0=1,dn=1,x0=0,y0=0,z0=0,instrument='G'):
+def gen_sources(Dlist,L,n,n0=1,x0=0,y0=0,z0=0,axis=0,direction=1):
+    lines = []
+
+    lines1 = add_sources(Dlist,n0,x0,y0,z0,sign=1*direction,axis=axis)
+
+    if axis==0:
+        lines2 = add_sources(Dlist,n0+len(Dlist),x0+(n-1)*L,y0,z0,sign=-1*direction,axis=axis)
+    if axis==1:
+        lines2 = add_sources(Dlist,n0+len(Dlist),x0,y0+(n-1)*L*direction,z0,sign=-1*direction,axis=axis)
+    lines = lines1+lines2
+
+    return lines
+
+def gen_S123(num,x0=0,y0=0,z0=0,axis=0,direction=1):
+    L = 3
+    n = 16
+    lines = gen_sources([5,8,11],L,n,n0=num,x0=x0,y0=y0,z0=z0,axis=axis,direction=direction)
+    return lines
+    
+def gen_line(n,L,n0=1,dn=1,x0=0,y0=0,z0=0,instrument='G',axis=0,direction=1):
     #create a line with n elements, spaced by L
     #by default, the line is oriented along x
     table = []
     header = ['#','X','Y','Z']
     table.append(header)
     for i,num in enumerate(range(n0,n*dn+n0,dn)):
-        line = [instrument+'_'+ndigit(num),i*L+x0,y0,z0]
+        if axis==0:
+            line = [instrument+'_'+ndigit(num),direction*i*L+x0,y0,z0]
+        if axis==1:
+            line = [instrument+'_'+ndigit(num),x0,direction*i*L+y0,z0]
+
         table.append(line)
     return table
 
@@ -57,9 +79,9 @@ def ndigit(i,n=2):
     else:
         return str(i)
     
-def add_lines(table,lines):
+def add_lines(table,lines,n0=0):
     #sort the lines by x position ??
-    for line in lines:
+    for line in lines[n0:]:
         table.append(line)
     return table
 
@@ -68,7 +90,7 @@ def exemple1():
     L=5
     D=10
     table = gen_line(n,L,instrument='G')
-    lines = gen_sources(D,L,n)
+    lines = gen_sources([D],L,n)
     table = add_lines(table,lines)
     return table
 
@@ -79,37 +101,12 @@ def exemple2():
     D=20
     table = gen_rectangle(n,n,L,L,instrument='G')
 
-    table = add_lines(table,gen_sources(D,L,n))
-    table = add_lines(table,gen_sources(D,L,n,n0=3,y0=ni*L))
-    table = add_lines(table,gen_sources(D,L,n,n0=5,axis=1))
-    table = add_lines(table,gen_sources(D,L,n,n0=7,axis=1,x0=ni*L))
+    table = add_lines(table,gen_sources([D],L,n))
+    table = add_lines(table,gen_sources([D],L,n,n0=3,y0=ni*L))
+    table = add_lines(table,gen_sources([D],L,n,n0=5,axis=1))
+    table = add_lines(table,gen_sources([D],L,n,n0=7,axis=1,x0=ni*L))
     return table
-
-def sag24_0206_Geophone1():
-    n=16
-    L=3
-    D = 1
-    table = gen_line(n,L,n0=1,instrument='G')
-    table.append(gen_point(1,-1,0,0,instrument='S'))
-    table.append(gen_point(2,15,0,0,instrument='S'))
-    table.append(gen_point(3,33,0,0,instrument='S'))
-    table.append(gen_point(4,46,0,0,instrument='S'))
-    return table
-
-def sag24_0206_Geophone2():
-    n=8
-    L=6
-    D = 1
-    table = gen_line(n,L,n0=1,dn=2,instrument='G')
-    table = add_lines(table,gen_line(n,L,n0=2,dn=2,x0=3,y0=-5.2,instrument='G')[1:])
-
-#    table.append(gen_point(1,-1,0,0,instrument='S'))
-#    table.append(gen_point(2,15,0,0,instrument='S'))
-#    table.append(gen_point(3,33,0,0,instrument='S'))
-#    table.append(gen_point(4,46,0,0,instrument='S'))
-    return table    
-
 
 if __name__ == '__main__':
-    table = exemple2()
+    table = sag24_0210_Geophone_line1()
     display.show(table)
