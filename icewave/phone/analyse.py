b819e774836bbc4934301e9e48d699fd7ebe8ba1
sperrard <stephane.perrard@espci.fr>
2024-10-17 22:46:07 +0200

    update time_interval function

--- icewave/phone/analyse.py
+++ icewave/phone/analyse.py
@@ -143,16 +143,17 @@ def find_measure_interval(data,var='a',Dt=5,S0=1,display=False):
     if N>3:        
         indices = np.where(S<S0)[0]
         inds = list(np.diff(indices)>1)
-        inds1 = [True]+inds
-        indices_cut = np.asarray(indices)[inds1]
-        i0 = np.argmax(np.diff(indices_cut))
-        imin = indices_cut[i0]
 
-        inds2 = inds+[True]
-        indices_cut = np.asarray(indices)[inds2]
-        i0 = np.argmax(np.diff(indices_cut))
-        imax = indices_cut[i0+1]
-        if len(indices)>=2:
+        if len(inds)>=2:
+            inds1 = [True]+inds
+            indices_cut = np.asarray(indices)[inds1]
+            i0 = np.argmax(np.diff(indices_cut))
+            imin = indices_cut[i0]+1
+
+            inds2 = inds+[True]
+            indices_cut = np.asarray(indices)[inds2]
+            i0 = np.argmax(np.diff(indices_cut))
+            imax = indices_cut[i0+1]-1
             data[var+'i0']=imin*n
             data[var+'i1']=imax*n
             data[var+'t0']=t[imin*n]
