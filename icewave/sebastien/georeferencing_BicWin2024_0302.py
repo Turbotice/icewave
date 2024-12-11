69738c93e9c2649cb5874df5c6b80b5b1c3adada
skuchly <sebastien.kuchly@gmail.com>
2024-12-11 15:12:21 +0100

    Drone - georeferencing 0302 star array

--- icewave/sebastien/georeferencing_BicWin2024_0302.py
+++ icewave/sebastien/georeferencing_BicWin2024_0302.py
@@ -10,96 +10,159 @@ import os
 import numpy as np 
 import matplotlib.pyplot as plt 
 import pickle
-import cv2 as cv
 from datetime import datetime, time , timedelta
 import pytz
 import glob 
 import imageio as iio
+import cv2 as cv
+import h5py
+from scipy.io import loadmat
 
-import icewave.field.drone as drone 
-import icewave.field as field 
-import icewave.tools.rw_data as rw_data
+import icewave.tools.matlab2python as mat2py
 import icewave.drone.drone_projection as dp 
-import icewave.tools.datafolders as df
 
 #%%
 
-# import an image from a single day 
-date = '0302'
-year = '2024'
-drone_ID = 'bernache'
-flight_ID = '6-Calib_oblique_002'
+base = 'Y:/Banquise/Sebastien/Calibration_intrinseque/Calib_oblique_bernache_2/'
+filelist_tif = glob.glob(base + '*.tif')
+filelist_param = glob.glob(base + 'Param*.mat')
+filelist_POS = glob.glob(base  + 'POS*.mat')
 
-base = df.find_path('Hublot24')
-base_images = base.replace('Data','PIV_images')
-path2tiff = f'{base_images}/{date}/Drones/{drone_ID}/{flight_ID}/'
 
-film_list = os.listdir(path2tiff)
-film_ID = film_list[0]
-filelist_tiff = glob.glob(f'{path2tiff}/{film_ID}/*.tiff')
+fig_folder = 'C:/Users/sebas/OneDrive/Bureau/Article_BicWin2024/' + 'Figures_python_small_size/'
+if not os.path.isdir(fig_folder):
+    os.mkdir(fig_folder)
+    
+#%%
+
+# Parameters for plots
+font_size_medium = 20
+font_size_small = round(0.6*font_size_medium)
+plt.rc('font', size=font_size_medium)          # controls default text sizes
+plt.rc('axes', titlesize=font_size_medium)     # fontsize of the axes title
+plt.rc('axes', labelsize=font_size_medium)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=font_size_small)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=font_size_small)    # fontsize of the tick labels
+plt.rc('legend', fontsize=font_size_medium)    # legend fontsize
+plt.rc('figure', titlesize=font_size_medium)  # fontsize of the figure title
+
+fig_size = (12,9)
+img_quality = 100 # dpi to save images 
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', serif='Computer Modern')
 
 #%%
 
-frame = filelist_tiff[0]
-# load the image 
+i0 = 3
+frame = filelist_tif[i0]
+img_name = frame.split('\\')[-1]
+img_name = img_name.split('.tif')[0]
 
 img = cv.imread(frame)
 img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
-# img = np.transpose(img,axes = [1,0,2])
 
-#%% show image 
+
+#%% Load the .mat file of parameters and convert to a dictionary
+ 
+param_file = filelist_param[i0]
+
+with h5py.File(param_file, 'r') as fmat:
+    param_dict = {}
+    
+    print('Top-level keys : ', list(fmat.keys()))
+
+    param_dict = mat2py.mat_to_dict(fmat['param'],fmat['param'])
+
+# Load the .mat file of objects position (without h5py)
+
+POS_file = filelist_POS[i0]
+POS_matdict = loadmat(POS_file)
+POS = POS_matdict['POS']
+
+#%% Plot raw image and detected buoys 
+
 fig, ax = plt.subplots()
 ax.imshow(img)
+ax.plot(POS[:,1],POS[:,2],'o',markerfacecolor = 'r',markeredgecolor = 'k',markersize = 8)
+ax.set_xlabel(r'$x_p $',labelpad = 5)
+ax.set_ylabel(r'$y_p $',labelpad = 5)
+ax.set_aspect(1) # set aspect ratio to 1 
 
-#%% 
+figname = fig_folder + 'Pixel_coord_' + img_name
+plt.savefig(figname + '.pdf', bbox_inches='tight')
+plt.savefig(figname + '.svg', bbox_inches='tight')
 
-focale = 2700;
-h = 11.800; # drone height in meters 
-alpha_0 = 17.7*np.pi/180; # drone pitch angle to horizontal 
+#%% Georeference image and compute object position
+    
+focale = 2700
+[ny,nx,nc] = np.shape(img) 
 
-[ny,nx,nt] = np.shape(img) 
-
-x_edges = np.arange(1,nx + 2)
-y_edges = np.arange(1,ny+ 2)
+x_edges = np.arange(0,nx + 1)
+y_edges = np.arange(0,ny + 1)
 
 x0 = (nx + 1) / 2
 y0 = (ny + 1) / 2
 
-# define limits of the image (pixel indices)
-x_borders = [1,nx] 
-y_borders = [500,ny]
-
-mask_xedges = np.where((x_edges >= x_borders[0]) & (x_edges <= x_borders[1] + 1))[0]
-mask_yedges = np.where((y_edges >= y_borders[0]) & (y_edges <= y_borders[1] + 1))[0]
-
-x = x_edges[mask_xedges]
-y = y_edges[mask_yedges]
-
-
-sub_img = img[y_borders[0] - 1 : y_borders[1] , x_borders[0] - 1 : x_borders[1] ,:]
-sub_img = np.transpose(sub_img,[1,0,2])
-
-Xedges,Yedges = np.meshgrid(x,y,indexing = 'ij')
+Yedges,Xedges = np.meshgrid(y_edges,x_edges,indexing = 'ij')
 
 # compute real coordinates 
-Xreal,Yreal = dp.projection_real_space(Xedges,Yedges,x0,y0,h,alpha_0,focale)
+Xreal,Yreal = dp.projection_real_space(Xedges,Yedges,x0,y0,param_dict['H'],param_dict['alpha_0']*np.pi/180,focale)
 
+# compute object position
+POS_realx,POS_realy = dp.projection_real_space(POS[:,1],POS[:,2],x0,y0,
+                                               param_dict['H'],param_dict['alpha_0']*np.pi/180,focale)
 
-#%%
-
-fig, ax = plt.subplots()
-c = ax.pcolormesh(Xedges,np.flip(Yedges,1),sub_img[:,:,0],shading = 'auto')
-fig.colorbar(c,ax = ax)
 
 #%% Display georeferenced image 
 
-fig, ax = plt.subplots()
-c = ax.pcolormesh(Xreal,Yreal,sub_img[:,:,0],shading = 'auto', cmap = 'viridis')
-fig.colorbar(c,ax = ax)
-ax.set_xlabel(r'$x \quad \mathrm{(m)}$')
-ax.set_ylabel(r'$y \quad \mathrm{(m)}$')
+fig, ax = plt.subplots(figsize = fig_size)
+c = ax.pcolormesh(Xreal,Yreal,img[:,:,0],shading = 'auto', cmap = 'gray')
+ax.plot(POS_realx,POS_realy,'o',markerfacecolor = 'r',markeredgecolor = 'k',markersize = 10)
+ax.set_xlabel(r'$X \; \mathrm{(m)}$',labelpad = 5)
+ax.set_ylabel(r'$Y \; \mathrm{(m)}$',labelpad = 5)
+ax.set_aspect(1) # set aspect ratio to 1 
 
-#%% 
+c.set_rasterized(True)
+figname = fig_folder + 'Camera_coord_' + img_name
+plt.savefig(figname + '.pdf', bbox_inches='tight')
+plt.savefig(figname + '.svg', bbox_inches='tight')
+
+#%% Compute GPS position 
+
+# horizontal distance between center of metric coordinates and drone position 
+dist2drone = param_dict['H']/np.tan(param_dict['alpha_0']*np.pi/180)
+Lat0,Long0 = dp.LatLong_coords_from_referencepoint(param_dict['latitude'],param_dict['longitude'],
+                                                param_dict['azimuth'],dist2drone)
+
+rho,theta = dp.cart2pol(Xreal,Yreal)
+theta = theta*180/np.pi # angle with respect to the drone orientation 
+local_azimuth = param_dict['azimuth'] + 90 - theta 
+# GPS coordinates of all pixels
+Lat,Long = dp.LatLong_coords_from_referencepoint(Lat0,Long0,
+                                                local_azimuth,rho)
+
+#GPS coordinates of detected objects 
+POS_rho,POS_theta = dp.cart2pol(POS_realx,POS_realy)
+POS_theta = POS_theta*180/np.pi
+POS_azimuth = param_dict['azimuth'] + 90 - POS_theta
+POS_Lat,POS_Long = dp.LatLong_coords_from_referencepoint(Lat0,Long0,
+                                                POS_azimuth,POS_rho)
+
+#%% Plot figure with GPS coordinates 
+
+fig,ax = plt.subplots(figsize = fig_size)
+c = ax.pcolormesh(Long,Lat,img[:,:,0],shading = 'auto',cmap = 'gray')
+ax.plot(POS_Long,POS_Lat,'o',markerfacecolor = 'r',markeredgecolor = 'k',markersize = 10)
+ax.set_xlabel(r'Longitude $(^\circ)$',labelpad = 15)
+ax.set_ylabel(r'Latitude $(^\circ)$',labelpad = 15)
+ax.set_aspect(1/np.cos(Lat0*np.pi/180)) # scaling y/x
+
+
+c.set_rasterized(True)
+figname = fig_folder + 'GPS_coord_' + img_name
+plt.savefig(figname + '.pdf', bbox_inches='tight')
+plt.savefig(figname + '.svg', bbox_inches='tight')
 
 
 
@@ -108,3 +171,62 @@ ax.set_ylabel(r'$y \quad \mathrm{(m)}$')
 
 
 
+
+
+
+
+
+#%% Plot figure of tiles used for calibration 
+
+base_tile = 'Y:/Banquise/Sebastien/Article_BicWin2024/Figures_calibration_georef/'
+path2image = base_tile + 'im_0953.tiff'
+
+img_tile = cv.imread(path2image);
+img_tile = cv.cvtColor(img_tile,cv.COLOR_BGR2RGB)
+
+ny,nx,nc = np.shape(img_tile)
+
+
+#%%
+x_pix = np.arange(nx)
+x_pix = np.arange(ny)
+
+ymin = 80
+ymax = ny
+xmin = 1100
+xmax = 2590
+
+cut_img = img_tile[ymin:ymax,xmin:xmax,:]
+step_ticks = 400
+# labels of x and y ticks
+xticklabel = np.arange(xmin,xmax,step_ticks)
+yticklabel = np.arange(ymin,ymax,step_ticks)
+
+# position of x and y ticks 
+xtickposition = np.arange(np.shape(cut_img)[1],step = step_ticks)
+ytickposition = np.arange(np.shape(cut_img)[0],step = step_ticks)
+
+#%%
+xlabel = ['']*np.size(xticklabel)
+for i,label in enumerate(xticklabel) :
+    label = '$' + str(label) + '$'
+    xlabel[i] = label
+
+ylabel = ['']*np.size(yticklabel)
+for i,label in enumerate(yticklabel) :
+    label = '$' + str(label) + '$'
+    ylabel[i] = label
+
+#%%
+
+fig,ax = plt.subplots(figsize = fig_size)
+ax.imshow(cut_img)
+ax.set_xticks(xtickposition,labels = xlabel)
+ax.set_yticks(ytickposition,labels = ylabel)
+ax.set_xlabel(r'$x_p $',labelpad = 15)
+ax.set_ylabel(r'$y_p $',labelpad = 15)
+ax.set_aspect(1) # set aspect ratio to 1 
+
+figname = fig_folder + 'Tile_image'
+plt.savefig(figname + '.pdf', bbox_inches='tight')
+plt.savefig(figname + '.svg', bbox_inches='tight')
