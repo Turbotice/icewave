c797091b860c33983a2ea56f9806393a1d4039f2
skuchly <sebastien.kuchly@gmail.com>
2025-04-28 17:14:54 +0200

    Script for comparing different dispersion relations

--- icewave/sebastien/dispersion_relation_models.py
+++ icewave/sebastien/dispersion_relation_models.py
@@ -35,6 +35,72 @@ def Lamb_viscous(freq,nu) :
     
     return k_viscous
 
+
+def shallow_water(k,H,g = 9.81):
+    """ Shallow water dispersion relation """
+
+    omega = np.sqrt(g*k*np.tanh(k*H))
+    return omega
+
+def heavy_water(k,h_ice,H,ratio,g = 9.81):
+    """ Shallow water dispersion relation, taking into account sea ice weight"""
+    
+    omega = np.sqrt(g*k*np.tanh(k*H)/(1 + h_ice*k*ratio))
+    return omega
+
+def hydroelastic_squire(k,h_ice,H,E,nu,g = 9.81,rho_w = 1000):
+    """ Hydroelastic wave disperion relation, Squire version """
+    
+    D = E*h_ice**3/12/(1-nu**2)
+    omega = np.sqrt((g*k + D*k**5/rho_w)*np.tanh(k*H))
+    
+    return omega
+
+def heavy_hydroelastic(k,h_ice,H,E,nu,rho_ice,g = 9.81,rho_w = 1000):
+    """ Hydroelastic wave dispersion relation, taking into account ice weight"""
+    
+    D = E*h_ice**3/(12*(1-nu**2))
+    ratio = rho_ice/rho_w
+    omega = np.sqrt((g*k + D*k**5/rho_w)*np.tanh(k*H)/(1 + h_ice*k*ratio))
+    
+    return omega
+    
+    
+#%% Plot all disperion relations for a given set of parameters
+
+E = 3.5e9 # ice Young modulus
+nu = 0.3 # ice Poisson coefficient
+rho_ice = 917 # ice density
+ratio = rho_ice/1e3
+h_ice = 0.1 # ice thickness
+
+H = 100 # water depth 
+
+k = np.linspace(0.1,3,100)
+omega_water = shallow_water(k, H)/2/np.pi
+omega_heavy_water = heavy_water(k, h_ice, H, ratio)/2/np.pi
+omega_squire = hydroelastic_squire(k,h_ice,H,E,nu)/2/np.pi
+omega_heavy_squire = heavy_hydroelastic(k, h_ice, H, E, nu, rho_ice)/2/np.pi
+
+fig, ax = plt.subplots()
+ax.plot(k,omega_water,label = 'shallow water')
+ax.plot(k,omega_squire,label = 'squire')
+ax.plot(k,omega_heavy_water,label = 'heavy water')
+ax.plot(k,omega_heavy_squire,label = 'heavy squire')
+
+
+ax.set_xlabel(r'$k \; \mathrm{(rad.m^{-1})}$')
+ax.set_ylabel(r'$f \; \mathrm{(Hz)}$')
+
+ax.set_ylim([0,2]) # set range of frequencies 
+
+ax.legend()
+
+
+
+
+
+
 #%%
 
 freq = np.linspace(0.2,1,100)
