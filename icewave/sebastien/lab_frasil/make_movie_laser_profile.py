1e3ebd91443d0b1e916ecacd63e77043c8488c8e
skuchly <sebastien.kuchly@gmail.com>
2025-05-23 18:32:33 +0200

    Update : ProcessPoolExecutor

--- icewave/sebastien/lab_frasil/make_movie_laser_profile.py
+++ icewave/sebastien/lab_frasil/make_movie_laser_profile.py
@@ -5,19 +5,48 @@ Created on Fri May 23 14:59:46 2025
 @author: sebas
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 import pickle
-import os
 import glob
 
-import icewave.tools.matlab2python as mat2py
-import icewave.tools.matlab_colormaps as matcmaps
-import icewave.sebastien.set_graphs as set_graphs
-import icewave.tools.Fourier_tools as FT
+from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm
 
+# import icewave.sebastien.set_graphs as set_graphs
+
+def set_matplotlib(style):
+    # fig_size = (6.4,4.8) default fig_size 
+    
+    if style == 'single':
+        fig_size = (8,6)
+        font_size_medium = 22
+    elif style == 'double':
+        fig_size = (8,6)
+        font_size_medium = 30
+    elif style == 'square':
+        fig_size = (12,9)
+        font_size_medium = 20
+    else:
+        
+        raise ValueError('The chosen style is not defined')
+        
+    plt.rcParams['figure.figsize'] = fig_size
+
+    font_size_small = round(0.75*font_size_medium)
+    plt.rc('font', size=font_size_medium)          # controls default text sizes
+    plt.rc('axes', titlesize=font_size_medium)     # fontsize of the axes title
+    plt.rc('axes', labelsize=font_size_medium)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=font_size_small)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=font_size_small)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=font_size_medium)    # legend fontsize
+    plt.rc('figure', titlesize=font_size_medium)  # fontsize of the figure title
+
+    plt.rcParams["axes.labelpad"] = 5
+    # plt.rc('text', usetex=True)
+    # plt.rc('font', family='serif', serif='Computer Modern')
+    return
 
 def animation_profile(data,x,t,nb_frames,time_interval):
     
@@ -32,8 +61,8 @@ def animation_profile(data,x,t,nb_frames,time_interval):
          
     Output : - ani: matplotlib animation object"""
     
-    set_graphs.set_matplotlib_param('single')
-    fig, ax = plt.subplots(figsize = (12,9))
+    set_matplotlib('single')
+    fig, ax = plt.subplots()
     j0 = 0 # initial time index 
     line = ax.plot(x,data[:,j0])[0]
     
@@ -55,29 +84,49 @@ def animation_profile(data,x,t,nb_frames,time_interval):
     
     return ani
 
+def process_file(args):
+    
+    file2load,path2data,nb_periods,slow_ratio = args
+    
+    with open(file2load,'rb') as pf:
+        data = pickle.load(pf)
+
+    h = data['h']
+    f_ex = data['f_ex']
+    amplitude = data['amplitude']
+
+    suffixe = f'h_{h}_fex_{f_ex}_amp_{amplitude}mm'
+    suffixe = suffixe.replace('.','p')
+
+    nb_frames = int(nb_periods*data['SCALE']['facq_t']/data['f_ex'])
+    ani = animation_profile(data['spatio']*1e3,data['x'],data['t'],nb_frames,
+                            time_interval = 1e3*slow_ratio/data['SCALE']['facq_t'])
+
+    file2save = f'{path2data}Laser_extraction/animation_{suffixe}.mp4'
+    ani.save(file2save)
+    
+    return 
+
 #%%
+def main():
+    h = 10.0 # frasil thickness 
+    date = '2024_07_11'
+    base = '/media/turbots/Backup25/'
+    path2data = f'{base}Aurore_frasil/{date}_e_{h}mm_laser/'
+    
+    filelist = glob.glob(f'{path2data}Laser_extraction/scaled_laser_structure_*.pkl')
+    
+    nb_periods = 20
+    slow_ratio = 5
+    
+    args_list = [(file,path2data,nb_periods,slow_ratio) for file in filelist]
+    
+    # for args in args_list:
+    #     process_file(args)
+        
+    with ProcessPoolExecutor(max_workers=30) as executor:
+        list(tqdm(executor.map(process_file, args_list), total=len(args_list)))
 
-h = 10.0 # frasil thickness 
-date = '2024_07_11'
-path2data = f'U:/Aurore_frasil/{date}_e_{h}mm_laser/'
+if __name__ == '__main__':
+    main()
 
-filelist = glob.glob(f'{path2data}Laser_extraction/scaled_laser_structure_*.pkl')
-
-idx_experiment = 5
-file2load = filelist[idx_experiment]
-print(file2load)
-with open(file2load,'rb') as pf:
-    data = pickle.load(pf)
-
-#%% Compute animation and save it 
-
-nb_periods = 20
-slow_ratio = 5
-nb_frames = int(nb_periods*data['SCALE']['facq_t']/data['f_ex'])
-ani = animation_profile(data['spatio']*1e3,data['x'],data['t'],nb_frames,
-                        time_interval = 1e3*slow_ratio/data['SCALE']['facq_t'])
-
-file2save = f'{path2data}Laser_extraction/animation_test.mp4'
-print('Saving animation...')
-ani.save(file2save)
-print('Animation saved')
