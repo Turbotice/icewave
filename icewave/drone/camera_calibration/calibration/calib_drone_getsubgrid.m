21e91656abb0c984907705a202e702e1ab3a87cd
skuchly <sebastien.kuchly@gmail.com>
2025-12-19 17:34:05 +0100

    camera calibration scripts

---
+++ icewave/drone/camera_calibration/calibration/calib_drone_getsubgrid.m
@@ -0,0 +1,64 @@
+function M = calib_drone_getsubgrid(im,Xout,Yout,name)
+% 
+
+
+
+figure(1) % show image 
+hold off
+imshow(im)
+hold on
+plot(Xout,Yout,'ro','markersize',6,'MarkerFaceColor','r')
+
+b=400;
+s=[];
+c=0;
+n = 9;
+
+M={};
+
+nx = 3840;
+ny = 2160;
+
+while isempty(s)
+    c=c+1;
+    [xc,yc] = getpts(gcf); % get center coordinate of subgrid
+    axis([xc-b xc+b,yc-b yc+b])
+
+    plot(xc,yc,'g+','markersize',8,'MarkerFaceColor','g')
+
+    [xc,yc] = getpts(gcf); % get more precise coordinate of subgrid center
+
+    plot(xc,yc,'go','markersize',8,'MarkerFaceColor','g')
+
+    %select axis of the subgrid
+    [xh,yh] = getpts(gcf);
+        plot(xh,yh,'go','markersize',8,'MarkerFaceColor','g')
+
+    [xv,yv] = getpts(gcf);
+    plot(xv,yv,'go','markersize',8,'MarkerFaceColor','g')
+
+    p = [xc,yc];
+    u = [xh-xc,yh-yc];
+    v = [xv-xc,yv-yc];
+    
+    eps = 0.1;
+    corners{1} = p+(u+v)*(1+1.5*eps);
+    corners{2} = p+(u-v)*(1+eps);
+    corners{3} = p-(u-v)*(1+1.5*eps);
+    corners{4} = p-(u+v)*(1+eps);
+
+    [Xin,Yin] = calib_drone_select_subgrid(Xout,Yout,corners); % get all points within corners 
+    
+    plot(Xin,Yin,'bo','markersize',8,'MarkerFaceColor','b')
+
+    M(c).m.Xsub = Xin;
+    M(c).m.Ysub = Yin;
+
+    M(c).m.corners = corners; % initially commented by Stephane 
+    
+    save(['transient_' name],'M')
+    axis([1,nx,1,ny])
+    getframe();
+
+    s=input('Select another subgrid ?')
+end
---
+++ icewave/drone/camera_calibration/im_2_binary.m
@@ -0,0 +1,88 @@
+clear all
+close all
+
+%%
+% f=rdir('Images/*.tiff')
+path2tiff = 'W:/Banquise/Calibration_PMMH_2026/20251216/calib_video_4K/frame_selection/';
+f = dir([path2tiff '*.tiff']);
+
+path2save = [ path2tiff 'Binarisation/'];
+if ~isfolder(path2save) 
+    mkdir(path2save)
+end
+
+%%
+for i=1:length(f)
+    
+    disp(strcat(num2str(i),'/',num2str(length(f))))
+    
+    file2load = f(i).name;
+    img=imread([f(i).folder '/' file2load]);
+    filename=file2load;
+    filename=filename(end-8:end-5);
+    filename=strcat(path2save,filename,'.mat');
+
+%filename=strcat('Binarisation/',filename(1:end-4),'mat');
+%imshow(img)
+
+    %filtrage en fourier
+    padding=4096;
+    Y=fftshift(fft2(img(:,:,1),padding,padding));
+    size_filt=40;
+    Y(padding/2+1-size_filt:padding/2+1+size_filt,padding/2+1-size_filt:padding/2+1+size_filt)=zeros;
+    
+    imgfilt=ifft2(ifftshift(Y));
+    imgfilt=imgfilt(1:size(img,1),1:size(img,2));
+    
+    imagesc(imgfilt)
+    
+    
+    se = strel('disk',3);
+    imgfilt=imgfilt+imtophat(imgfilt,se)-imbothat(imgfilt,se);
+    imagesc(imgfilt)
+
+    % Binarization
+    BW=imgfilt;
+    BW(find(imgfilt>-8))=1;
+    BW(find(imgfilt<=-8))=0;
+    
+    %T = adaptthresh(BW);
+    %BW = imbinarize(BW,T);
+    
+    %BW(find(imgfilt>-5))=1;
+    %BW(find(imgfilt<=-5))=0;
+    % 
+    SE = strel('disk',1);
+    % 
+    % %BW2=medfilt2(BW);
+    % BW2=imdilate(BW2,SE);
+    % BW2=imdilate(BW2,SE);
+    % 
+    % 
+     BW2=imerode(BW,SE);
+    % BW2=imerode(BW2,SE);
+    % 
+    BW2 = imfill(BW2,'holes');
+    imshow(BW2)
+    
+    %
+    BW=BW2;
+    save(filename,'BW')
+
+end
+
+
+%%
+clear
+f=rdir('Bin*/*.mat')
+
+for i=1:length(f)
+    load(f{i})
+    
+    imshow(BW)
+    title(num2str(i))
+    pause
+   
+    
+    end
+    
