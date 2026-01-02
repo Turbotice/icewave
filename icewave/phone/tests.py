2be231a5f7faef7f1033464a75c479b4e7c1dca9
sperrard <stephane.perrard@espci.fr>
2026-01-02 11:18:07 +0100

    Update Notebooks (smartphone server, phone data analysis)

--- icewave/phone/tests.py
+++ icewave/phone/tests.py
@@ -2,11 +2,14 @@ import glob
 import os
 import numpy as np
 import time
-
+import datetime 
 import icewave.phone.load as load
 
+from pprint import pprint
 global path
 path = 'Test_data_FP3/'
+global path_android
+path_android = '/storage/self/primary/Download/Gobannos/'
 
 def data_stats():
     #load all the files in the path folder
@@ -15,3 +18,27 @@ def data_stats():
     load.stat(data)
 
     return data
+
+def parse_files(date=None):
+    if date==None:
+        date = str(datetime.date.today())
+
+    filelist = glob.glob(path_android+date+'*.csv')
+
+    experiments={}
+    for filename in filelist:
+        get_start_time()
+    pprint(filelist)
+    #parse  per start time
+
+def get_start_time(filename):
+    filename = os.path.basename(filename)
+    filename = '20'+filename.split('20')[1]
+    '-'.join(filename.split('-')[:3])
+             
+#parse_files()
+#data_stats()
+
+
+
+
