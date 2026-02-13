f98cc574a9481e6c2adf1f14bd43ca4a9ee2c2bc
Vasco <zanchivasco@gmail.com>
2026-02-12 21:45:30 -0300

    ir analysis

---
+++ icewave/vasco/field/temperatures/open_temperatures_csv.py
@@ -0,0 +1,18 @@
+#%%
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#%%
+gps_num = 1
+
+date = '20260210'
+
+waypoint = '049'
+
+path2data = f'F:/bicwin26/Summary/temperature_profiles/'
+filename = path2data + f'{date}_gps{gps_num}_waypoint{waypoint}.csv'
+
+df = pd.read_csv(filename)
