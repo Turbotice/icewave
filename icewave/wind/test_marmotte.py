9c51dccaa78306ace7388a5509f07fe5536ae965
Hugo JACQUET <hugo.jacquet1@univ-grenoble-alpes.fr>
2026-02-17 21:15:03 +0100

    added: tests for thermistor and surface temperature

---
+++ icewave/wind/test_marmotte.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from read_marmotte import *
+
+
+# Get the file with the command:
+# scp -r -P 8022 u0_a236@192.168.1.184:/data/data/com.termux/files/home/git/pytruche/codes/marmotte .
+
+# file
+file_path = "/home/jacqhugo/BicWin26/icewave/icewave/wind/marmotte/"
+filename = "20260217_141524.203_marmotte.txt"
+
+# .txt -> .nc
+ds = parse_marmotte(file_path+filename)
+
+# plot
+fig, ax = plt.subplots(1,1,figsize = (3,3), constrained_layout=True, dpi=100)
+ax.plot(ds.time, ds.IRSensor_T, label='Ts')
+ax.plot(ds.time, ds.Airtemp, label='Airtemp')
+ax.plot(ds.time, ds.IRTemp_C, label='Ts corrected' )
+ax.set_ylabel('°C')
+ax.set_xlabel('Time')
+plt.legend()
+
+plt.show()
