ee1b2ca499322021c548fd7bb76b55c2721fe86a
Hugo JACQUET <hugo.jacquet1@univ-grenoble-alpes.fr>
2026-02-17 21:14:35 +0100

    cleanup

--- icewave/wind/sensorIR120.py
+++ icewave/wind/sensorIR120.py
@@ -1,9 +1,22 @@
+"""
+Functions to compte a surface temperature from a flux measured by the sensor IR120
+
+This is following the documentation of the IR120 by Campbell
+https://s.campbellsci.com/documents/eu/manuals/ir100_ir120%20-%20708.pdf
+
+If you use your own IR120, you will need to adapt the callibration coefficient
+"""
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-def T_t(R): 
+def compute_Rt(Va, V0, R1):
+    return R1*Va/(V0-Va)
+
+def compute_Tt(R): 
     """
-    Thermistor temperature (Kelvin)
+    Thermistor temperature (Celsius)
     """
 
     # Calibration constants
@@ -11,14 +24,45 @@ def T_t(R):
     B = 2.212819E-4
     C = 1.333627E-7
     
-    return 1/(A+B*np.log(R)+C*np.log(R)**3)
+    return 1/(A+B*np.log(R)+C*np.log(R)**3) - 273.15
+
+def temperature_compensation(IRSensor_Volt, IRSensorCan_temp):
+    return IRSensor_Volt * 1.0004**(IRSensorCan_temp - 25)
+
+def compute_energy(IRSensorVolt_TC):
+    # Calibration constants
+    X = 8.015848E-6
+    Y = 2.985651E-1
+    Z = 3.024548E-1
+
+    return (X*IRSensorVolt_TC**2 
+            + Y*IRSensorVolt_TC
+            + Z)
+
+def add_body_sensor_E(IRSensor_E, IRSensorCan_Temp):
+    """
+    Takes the absolute energy measured, and correct it from body temp.
+    Return emission from the surface (W/m2)
+    """
+    sig = 5.67e-8 # stefan boltzman constant
+    return (IRSensor_E + sig * (IRSensorCan_Temp + 273.15)**4)
 
 
-R = np.arange(50, 200)
-fig, ax = plt.subplots(1,1,figsize = (3,3), constrained_layout=True, dpi=100)
-ax.plot(R, T_t(R*1000))
-ax.hlines(273, 0, 1000, color='gray', ls='--')
-ax.set_ylabel('T (K)')
-ax.set_xlabel('R (kOhms)')
-ax.set_xlim([50,200])
-plt.show()
+def Corrected_temp(IRSensor_T4, Airtemp, emissivity=0.95):
+    """
+    Computes a temperature in Celsius, correct from air temperature and emissivity
+    """
+    return ((IRSensor_T4 - ( (Airtemp + 273.15)**4*(1-emissivity) ))/emissivity )**0.25 - 273.15
+
+def bits_to_tension(D, N, Alim):
+    return D/N*Alim
+
+
+# Vectorized versions
+vect_bits_to_tension = np.vectorize(bits_to_tension, excluded=["arg1", "arg2"])
+vect_compute_Rt = np.vectorize(compute_Rt, excluded=['arg1', 'arg2'])
+v_compute_Tt = np.vectorize(compute_Tt)
+v_temperature_compensation = np.vectorize(temperature_compensation)
+v_compute_energy = np.vectorize(compute_energy)
+v_add_body_sensor_E = np.vectorize(add_body_sensor_E)
+v_Corrected_temp = np.vectorize(Corrected_temp, excluded=['arg2'])
--- icewave/wind/todos_wind.md
+++ icewave/wind/todos_wind.md
@@ -28,3 +28,31 @@ aqua ?)
 ## Methods
 
 - periode de moyenne ?
+
+
+
+
+
+## Le mat
+
+voir git pytruche
+
+nmap: liste les reseaux dispo en wifi
+ifconfig
+
+les anémo: il faut sudo le programme pour le lancer (le brancher ca suffit pas)
+le lidar: brancher c'est ok, ca tourne direct
+
+
+accès à la pi5 par ssh:
+- par zerotier (réseau privé) si le téléphone a la 4g
+- ssh sur le téléphone qui est la bas (la pi5 est connecté à lui)
+
+quand sur connecté sur le téléphone:
+trouver ip de la pi5 avec nmap puis se connecter à la pi5
+
+
+pi5: za1, zaza
+reste: turbots, turbotice!
+
+La pi qui est au shack est la meme que la bas, on peut test la manip au chaud
